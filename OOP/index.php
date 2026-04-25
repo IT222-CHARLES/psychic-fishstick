588776aa8e4baa02023cb73ac67e67c513c1ec83
jrnibs <johnnibato13@gmail.com>
2026-04-25 14:54:36 +0800

    lesson MVC

--- OOP/index.php
+++ OOP/index.php
@@ -36,6 +36,10 @@
     if($bank->verifyCustomer($customer1)){
         echo $customer1->getName(). " is a customer";
     }
+    echo "<br>";
+    if($bank->verifyCustomer($customer2)){
+        echo $customer2->getName(). " is a customer";
+    }
     //transaction
 
     $bank->processTransaction($customer1->getAccounts()[0]['bankAccount'],1000);
@@ -64,54 +68,6 @@
         }
     }
 
-    echo $saveAccount->addInterest();
-    echo $saveAccount->getBalance();
-    
-    $account = new BankAccount(100);
-    // $account->setAccountnumber('123');
-    // $account->balance = "100";
-
-    // $account->deposit(100);
-    // $account->deposit(50);
-    // $account->withdraw(250);
-    // $account->deposit(150);
-    //var_dump($account->balance);
-    $account->transaction(200,'deposit');
-    $account->transaction(50,'withdraw');
-
-    echo "<h2>Account Details</h2>";
-    echo "Account Number: ". $account->getAccountnumber() . "<br>";
-    echo "Balance : Php ". $account->getBalance() . "<br>";
-
-    $account2 = new BankAccount();
-    // $account2->setAccountnumber('222');
-    // $account2->balance = 300;
-    // $account2->deposit(50);
-    // $account2->deposit(50)->deposit(50)->withdraw(250);
-    $account2->transaction(200,'deposit');
-    $account2->transaction(50,'withdraw');
-
-    echo "<h2>Account Details</h2>";
-    echo "Account Number: ". $account2->getAccountnumber() . "<br>";
-    echo "Balance : Php ". $account2->getBalance() . "<br>";
-
-    ?>
-
-    <h3>Customer</h3>
-    <?php
-    $customer = new Customer();
-    $customer->setName("JOHN DOE");
-    $customer->setName(" JOHN NIBATO ");
-
-    echo "Customer Name: ".$customer->getName()."<br>";
-
-    $bank = new Bank();
-    $bank->addCustomer("JOHN");
-    $bank->addCustomer("CARLO");
-
-    foreach($bank->getCustomers() as $account){
-        echo "Customer Name ". $account->getName()."<br>";
-    }
     ?>
 </body>
 </html>
